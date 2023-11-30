ad38094f8f016c642999d5713bdd1a4e1ce31019
namber0 <namnb2402@gmail.com>
2023-11-30 19:15:29 +0700

    Fix: Turn off welcom message if a file is opened

--- name.c
+++ name.c
@@ -269,7 +269,7 @@ void editorDrawRows(struct abuf* ab)
     {
         if (y >= E.numrows)
         {
-            if (y == E.screenrows / 3)
+            if (E.numrows == 0 && y == E.screenrows / 3)
             {
                 char welcome[80];
                 int welcomelen = snprintf(welcome, sizeof(welcome), "Unnamed Editor - Version %s", VERSION);
