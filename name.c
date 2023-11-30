5574b297dbcba9048bc394ec342a0c4d955838ee
namber0 <namnb2402@gmail.com>
2023-11-30 19:48:05 +0700

    Fix horizontal scrolling cursor positioning

--- name.c
+++ name.c
@@ -357,7 +357,7 @@ void editorRefreshScreen()
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1); // reposition the cursor by subtracting rowoff with cy
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1); // reposition the cursor by subtracting rowoff with cy and coloff with cx
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
